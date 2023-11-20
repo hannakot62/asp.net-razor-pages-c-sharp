65a898159b2e030573106cfbdb6a7610d4a9554b
Hanna <93535175+hannakot62@users.noreply.github.com>
2023-11-20 11:45:54 +0300

    Add files via upload

---
+++ Data/MySQLDataAccess.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pic5.Data
+{
+    public class MySqlDataAccess
+    {
+        private readonly string _connectionString;
+
+        public MySqlDataAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<T> LoadData<T>(string sql, object parameters = null)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<T>(sql, parameters).ToList();
+            }
+        }
+
+        public void SaveData<T>(string sql, T parameters)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                connection.Execute(sql, parameters);
+            }
+        }
+
+        public void UpdateData<T>(string sql, T parameters)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                connection.Execute(sql, parameters);
+            }
+        }
+
+        public void DeleteData<T>(string sql, T parameters)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                connection.Execute(sql, parameters);
+            }
+        }
+
+
+    }
+
+}
---
+++ Data/PurchaseData.cs
@@ -0,0 +1,50 @@
+﻿using pic5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pic5.Data
+{
+    public class PurchaseData
+    {
+        private  MySqlDataAccess _dataAccess;
+
+        public PurchaseData(string connectionString)
+        {
+            _dataAccess = new MySqlDataAccess(connectionString);
+        }
+
+        public List<Purchase> GetPurchases()
+        {
+            string sql = "SELECT * FROM purchases";
+            return _dataAccess.LoadData<Purchase>(sql);
+        }
+
+        public void SavePurchase(Purchase purchase)
+        {
+            string sql = "INSERT INTO purchases (Item, Price, DepartmentId) VALUES (@Item, @Price, @DepartmentId)";
+            _dataAccess.SaveData(sql, purchase);
+        }
+
+        public void DeletePurchaseById(int id)
+        {
+            string sql = "DELETE FROM purchases WHERE Id = @Id";
+            _dataAccess.SaveData(sql, new { Id = id });
+        }
+    
+        public void UpdatePurchase(Purchase purchase)
+        {
+            string sql = "UPDATE purchases SET item=@item, price=@price, departmentId=@departmentId WHERE id = @id";
+            _dataAccess.UpdateData(sql, purchase);
+        }
+
+
+        public Purchase GetPurchaseById(int id)
+        {
+            string sql = "SELECT * FROM purchases WHERE id = @Id";
+            return _dataAccess.LoadData<Purchase>(sql, new { Id= id}).FirstOrDefault();
+        }
+    }
+
+}
